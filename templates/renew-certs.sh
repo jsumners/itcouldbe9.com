bf02d2c3e43486262a5a84735a26b7c443356e4d
James Sumners <james@sumners.email>
2022-12-11 18:16:03 -0500

    Fix rewnew-certs script

--- templates/renew-certs.sh
+++ templates/renew-certs.sh
@@ -25,29 +25,4 @@ cat ${LEGO_CERTS}/{{ domain.cert_name }}.crt \
 
 {% endfor %}
 
-lego --eab \
-  --kid "{{ zerossl_kid }}" \
-  --hmac "{{ zerossl_hmac }}" \
-  --domains "lt.smurf.pw" \
-  --domains "*.lt.smurf.pw" \
-  --http --http.port :9999 \
-  --dns gandiv5 \
-  --email "{{ zerossl_email }}" \
-  --accept-tos \
-  --path /root/.lego \
-  --server https://acme.zerossl.com/v2/DV90 \
-  renew
-
-lego --eab \
-  --kid "{{ zerossl_kid }}" \
-  --hmac "{{ zerossl_hmac }}" \
-  --domains "sats.smurf.pw" \
-  --http --http.port :9999 \
-  --dns gandiv5 \
-  --email "{{ zerossl_email }}" \
-  --accept-tos \
-  --path /root/.lego \
-  --server https://acme.zerossl.com/v2/DV90 \
-  renew
-
 sv reload haproxy
