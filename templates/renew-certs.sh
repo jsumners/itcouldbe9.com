88d4388e02d007c802ae6b12b312879424464d64
James Sumners <james@sumners.email>
2023-01-23 10:29:23 -0500

    Drop ZeroSSL

--- templates/renew-certs.sh
+++ templates/renew-certs.sh
@@ -6,18 +6,15 @@ export GANDIV5_API_KEY={{ gandi_api_key }}
 LEGO_CERTS=/root/.lego/certificates
 
 {% for domain in tls_cert_domains %}
-lego --eab \
-  --kid "{{ zerossl_kid }}" \
-  --hmac "{{ zerossl_hmac }}" \
+lego \
 {% for san in domain.sans %}
   --domains "{{ san }}" \
 {% endfor %}
   --http --http.port :9999 \
   --dns gandiv5 \
-  --email "{{ zerossl_email }}" \
+  --email "{{ letsencrypt_email }}" \
   --accept-tos \
   --path /root/.lego \
-  --server https://acme.zerossl.com/v2/DV90 \
   --dns.resolvers 1.1.1.1 \
   renew
 cat ${LEGO_CERTS}/{{ domain.cert_name }}.crt \
