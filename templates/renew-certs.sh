43cae08e03aa2879f247f51144356cffe906ee17
James Sumners <git@zifbang.com>
2023-10-26 10:58:48 -0400

    Update acme script

--- templates/renew-certs.sh
+++ templates/renew-certs.sh
@@ -16,7 +16,8 @@ lego \
   --accept-tos \
   --path /root/.lego \
   --dns.resolvers 1.1.1.1 \
-  renew
+  renew \
+  --days 30
 cat ${LEGO_CERTS}/{{ domain.cert_name }}.crt \
   ${LEGO_CERTS}/{{ domain.cert_name }}.key \
   > /opt/certs/{{ domain.cert_name }}.pem
